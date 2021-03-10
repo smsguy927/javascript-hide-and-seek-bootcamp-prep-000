55af25f1d826610824a83990ada500f450d92681
smsguy927 <smsguy927@gmail.com>
2021-03-10 11:28:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -22,10 +22,10 @@ function increaseRankBy(n) {
     `.ranked-list`s by `n`. (You might need to make use of [`parseInt()`][parseint]
   */
   const rankedLists = document.querySelectorAll('.ranked-list');
-  
+
     for (let i = 0, l = rankedLists.length; i < l; i++) {
       let children = rankedLists[i].children;
-  
+
       for (let j = 0, k = children.length; j < k; j++) {
         children[j].innerHTML = parseInt(children[j].innerHTML) + n;
       }
