10f1cc37cc50f0789389c6131b4fec3671808d77
smsguy927 <smsguy927@gmail.com>
2021-03-10 11:11:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ function increaseRankBy(n) {
   .querySelectorAll('ul.ranked-list li');
 
 for (let i = 0; i < lis.length; i++) {
-  lis[i].innerHTML = (i + n + 1).toString();
+  console.log(lis[i].innerHTML);
   }
 }
 
